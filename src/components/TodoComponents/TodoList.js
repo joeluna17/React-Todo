12d5efd19c8b04c69135c36c3251e8d8eb1d5dd4
Joseph Luna <josephluna@Josephs-MBP.local>
2019-05-14 18:44:45 -0500

    intial commit for Day 1 challange yarn installed

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+const TodoList = props =>{ 
+    return (
+        <div className="todo-list-item-wrapper">
+            <h1> Todo List: MVP</h1>
+                <ul>
+                        <li>{
+                            return ({
+                                props.   
+                            })
+                        }</li>
+                </ul>
+        </div>
+
+    )
+}
+
+
+export default TodoList;
+
